49b925853d878e2d40bcd35600eeb189a017fc6e
jdkwer <hendervergara@gmail.com>
2025-06-09 10:02:09 +0800

    Activity 1

---
+++ database/seeders/StudentSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Student;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StudentSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        Student::factory()->count(10)->create();
+    }
+    
+}
